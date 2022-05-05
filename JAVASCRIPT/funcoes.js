5745a403f9bc484f0956862c99702aa9fd34d613
larissaborsari <larissaborsari@hotmail.com>
2022-05-05 13:27:41 -0300

    atualizacao de conteudo do bootcamp

--- JAVASCRIPT/funcoes.js
+++
@@ -1,5 +0,0 @@
-function soma(a,b) { //nos parenteses são declarados os parâmetros da função
-    return a + b; //se eu não for utilizar este valor da soma, apenas quero ver o resultado, posso utilizar console.log
-}
-
-soma (3,5);
