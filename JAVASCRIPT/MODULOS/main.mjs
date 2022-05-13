0d2cf5bc2f46616e3b8c732bdc9ebd7586ba2679
larissaborsari <larissaborsari@hotmail.com>
2022-05-12 21:28:22 -0300

    atualizacao

---
+++ JAVASCRIPT/MODULOS/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade('Larissa',24));
+console.log(mostraCidade('Larissa', 'Penha'));
+console.log(mostraHobby('Larissa', 'Ler'));
