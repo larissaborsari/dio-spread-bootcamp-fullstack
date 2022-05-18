e56745ba7e3ed163e6426229a7dc17b9e1e85423
larissaborsari <larissaborsari@hotmail.com>
2022-05-18 13:35:05 -0300

    atualizacao

--- TYPESCRIPT/SISTEMA ESTACIONAMENTO/script.ts
+++ TYPESCRIPT/SISTEMA ESTACIONAMENTO/script.ts
@@ -1,5 +1,41 @@
+interface Veiculo {
+    nome: string;
+    placa: string;
+    entrada: Date;
+}
+
 (function(){
     const $ = (query: string):HTMLInputElement | null => document.querySelector(query);
+
+    function patio() {
+        function ler() {}
+
+
+        function adicionar(veiculo: Veiculo) {
+            const row = document.createElement("tr");
+            row.innerHTML = `
+                <td>${veiculo.nome}</td>
+                <td>${veiculo.placa}</td>
+                <td>${veiculo.entrada}</td>
+                <td>
+                <button class="delete" data-placa="${veiculo.placa}">x</button>
+                </td>
+            `;
+
+            $("#patio")?.appendChild(row);
+        }
+
+
+        function remover() {}
+
+
+        function salvar() {}
+
+
+        function render() {}
+
+        return {ler, remover, adicionar, salvar, render}
+    }
 
     $("#cadastrar")?.addEventListener("click", () => {
         const nome = $("#nome")?.value;
@@ -9,5 +45,7 @@
             alert("Os campos nome e placa são obrigatórios!");
             return;
         }
+
+        patio().adicionar({nome,placa,entrada: new Date()})
     })
 })();
