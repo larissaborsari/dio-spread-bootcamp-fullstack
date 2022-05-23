e8b61ed42ac4acd4891240ea8dc2200efbac5a9b
larissaborsari <larissaborsari@hotmail.com>
2022-05-23 19:23:19 -0300

    atualizacao

---
+++ REACTJS/STATES E EFFECTS/aula-hooks/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './Compenents/App/App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
