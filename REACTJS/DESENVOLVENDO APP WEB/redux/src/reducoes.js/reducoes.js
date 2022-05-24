c8aa9d9cca1a86d2d235d5c8324cfb1ae7346370
larissaborsari <larissaborsari@hotmail.com>
2022-05-24 20:47:05 -0300

    atualizacao

---
+++ REACTJS/DESENVOLVENDO APP WEB/redux/src/reducoes.js/reducoes.js
@@ -0,0 +1,20 @@
+import { INCREMENT, DRECREMENT } from "../actions.js/actions";
+
+const initialState = {
+    count: 0
+}
+
+export function reducer(state = initialState, action) {
+    switch(action.type) {
+        case INCREMENT:
+            return {
+                count: state.count + 1
+            };
+        case DECREMENT:
+            return {
+                count: state.count - 1
+            };
+        default:
+            return state;
+    }
+}
