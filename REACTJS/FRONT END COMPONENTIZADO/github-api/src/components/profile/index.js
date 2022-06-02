333620c29ba35a18db53b3c377c63a0e69e4a6b7
larissa.borsari <larissaborsari@hotmail.com>
2022-06-02 19:52:44 -0300

    atualização

--- REACTJS/FRONT END COMPONENTIZADO/github-api/src/components/profile/index.js
+++ REACTJS/FRONT END COMPONENTIZADO/github-api/src/components/profile/index.js
@@ -4,24 +4,31 @@ import * as S from './styled';
 function Profile() {
   return (
     <S.Wrapper>
-        <div>
-            <img src="https://avatars.githubusercontent.com/u/81311347?v=4" alt="User Avatar"/>
+        <S.WrapperImage src="https://avatars.githubusercontent.com/u/81311347?v=4" alt="User Avatar"/>
+        <S.WrapperInfoUser>
+            <div>
             <h1>Larissa Borsari</h1>
-            <h3>Username: </h3>
-            <span>larissaborsari</span>
+            <S.WrapperUserName>
+                <h3>Username: </h3>
+                <a href="https://github.com/larissaborsari" target="_blank" rel="noreferrer">larissaborsari</a>
+            </S.WrapperUserName>
+             
         </div>
-        <div>
-            <h4>Followers: </h4>
-            <span>2</span>
+            <S.WrapperStatusCount>
+            <div>
+                <h4>Followers: </h4>
+                <span>2</span>
+                </div>
+            <div>
+                <h4>Starred: </h4>
+                <span>2</span>
             </div>
-        <div>
-            <h4>Starred: </h4>
-            <span>2</span>
-        </div>
-        <div>
-            <h4>Following: </h4>
-            <span>2</span>
-        </div>
+            <div>
+                <h4>Following: </h4>
+                <span>2</span>
+            </div>
+            </S.WrapperStatusCount>
+        </S.WrapperInfoUser>
     </S.Wrapper>
   );
 }
--- REACTJS/FRONT END COMPONENTIZADO/github-api/src/components/profile/styled.js
+++ REACTJS/FRONT END COMPONENTIZADO/github-api/src/components/profile/styled.js
@@ -2,5 +2,37 @@ import styled from 'styled-components';
 
 
 export const Wrapper = styled.div`
+    display: flex;
+    align-items: flex-start;
+`;
 
+export const WrapperInfoUser = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-between;
+    height: auto;
+`;
+
+export const WrapperStatusCount = styled.div`
+    display: flex;
+    align-items: flex-start;
+    div {
+        margin: 8px;
+    }
+`;
+
+export const WrapperUserName = styled.div`
+    display: flex;
+    align-items: center;
+    h3 {
+        margin-right: 8px;
+        text-align: center;
+    }
+`;
+
+export const WrapperImage = styled.img`
+    border-radius: 50%;
+    width: 200px;
+    margin: 8px;
 `;
